523e83e193d18fa7e531865685228dd5ccf0c05d
Adina Keller <adinakeller15@gmail.com>
2025-10-10 14:19:11 +0100

    plateau:made class for grid

--- test_the_plateau.py
+++ test_the_plateau.py
@@ -1,18 +1,4 @@
-from the_plateau import Input
 
-def test_changes_grid_input_to_int_tuple():
-    string_input = Input()
-    output = string_input.plateau_grid("5 5")
-    assert output == (5, 5)
-
-    string_input2 = Input()
-    output = string_input2.plateau_grid("PLATEAU5x5")
-    assert output == (5, 5)
-
-# def test_converts_starting_input_to_tuple():
-#     string_input = Input()
-#     output = string_input.rover_start("5 5 N")
-#     assert output == (5, 5, 'N')
 
 
 
--- the_plateau.py
+++ the_plateau.py
@@ -3,31 +3,4 @@ class Plateau:
         self.width = width
         self.height = height
 
-class Rover:
-    def __init__(self, x, y, compass_points):
-        self.across = x
-        self.up = y
-        self.compass_points = compass_points
 
-class Input:
-    def plateau_grid(self, input):
-        nums = '0123456789'
-        list = [chr for chr in input]
-        coordinates = []
-        for chr in list:
-            if chr in nums:
-                coordinates.append(int(chr))
-        width = coordinates[0]
-        height = coordinates[1]
-        return width, height
-    # print(plateau_grid("PLATEAU5x5"))
-    # print(plateau_grid("5 5"))   
-        
-
-    def rover_start(self, input):
-        list = input.split()
-        x = int(list[0])
-        y = int(list[1])
-        compass = list[2]
-        return x, y, compass
-    #print(rover_start('5 5 N'))
