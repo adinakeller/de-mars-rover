0253f6e7c9a9ee57553fb3a2bb8604cb81ed8444
Adina Keller <adinakeller15@gmail.com>
2025-10-12 16:22:02 +0100

    inputs: tests done

--- inputs.py
+++ inputs.py
@@ -2,20 +2,25 @@ from enums import Instruction, CompassDirection
 
 class Input:
     def parse_plateau(input):
+        if input == '':
+            return 'Please enter a plateau size'
+        
         nums = '0123456789'
         list = [chr for chr in input]
-        coordinates = []
-        for chr in list:
-            if chr in nums:
-                coordinates.append(int(chr))
+        coordinates = [int(chr) for chr in list if chr in nums]
+    
         width = coordinates[0]
         height = coordinates[1]
         return width, height
-    #print(parse_plateau("PLATEAU0x0"))
-    #print(parse_plateau("5 5"))   
+    
+#This Plateau has maximum (x, y) co-ordinates of (5, 5), 
+# and is therefore has 6 possible x and y values (0 - 5 for each).
+      
         
-
     def parse_rover(input):
+        if input == '':
+            return 'Please enter a starting point'
+        
         list = input.split()
         x = int(list[0])
         y = int(list[1])
@@ -25,12 +30,15 @@ class Input:
             direction = list[2]
         except ValueError:
             raise ValueError(f'Invalid direction: {list[2]}')
+        
         return x, y, direction
-    #print(parse_rover('5 5 N'))
-    #print(parse_rover('5 5 P'))
+    
 
     def parse_instructions(input):
         valid_letters = []
+        if input == '':
+            return 'Please enter instructions'
+        
         for chr in input:
             try:
                 Instruction(chr)
@@ -39,5 +47,6 @@ class Input:
                 continue
         return valid_letters
             
-        #return [chr for chr in input if Instruction(chr)]
-    #print(parse_instructions('LMLRMBM'))
+
+# create func that handles all inputs being empty??
+# what if rover inputs exceeds grid size??
--- test_inputs.py
+++ test_inputs.py
@@ -1,6 +1,12 @@
 import pytest # type: ignore
 from inputs import Input
 
+def test_returns_message_if_any_input_is_empty():
+    assert Input.parse_plateau('') == 'Please enter a plateau size'
+    assert Input.parse_rover('') == 'Please enter a starting point'
+    assert Input.parse_instructions('') == 'Please enter instructions'
+
+
 def test_changes_grid_input_to_int_tuple():
     assert Input.parse_plateau("5 5") == (5, 5)
     assert Input.parse_plateau("PLATEAU0x0") == (0, 0)
@@ -12,7 +18,8 @@ def test_raises_error_if_invalid_compass_point():
     with pytest.raises(ValueError, match="Invalid direction: Q"):
         Input.parse_rover("1 2 Q")
 
+
 def test_converts_input_to_instructions_list():
     assert Input.parse_instructions('LMLMR') == ['L', 'M', 'L', 'M', 'R']
     assert Input.parse_instructions('LTRBM') == ['L', 'R', 'M']
-
+    
