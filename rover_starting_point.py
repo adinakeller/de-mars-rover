c00045e7e4d7c0ce2985cfcf1ad82568f37430c7
Adina Keller <adinakeller15@gmail.com>
2025-10-19 19:22:16 +0100

    add __str__

--- rover_starting_point.py
+++ rover_starting_point.py
@@ -5,6 +5,9 @@ class Rover:
     def __init__(self, position):
         self.position = position
 
+    def __str__(self):
+        return f'Rover: {self.position}'
+        
 @dataclass
 class Position:
     x = int
--- the_plateau.py
+++ the_plateau.py
@@ -4,6 +4,9 @@ class Plateau:
     def __init__(self, grid):
         self.grid = grid
 
+    def __str__(self):
+        return f'Plateau: {self.grid}'
+
 @dataclass
 class PlateauSize:
     width = int
