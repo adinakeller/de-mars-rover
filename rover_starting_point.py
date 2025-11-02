a09f30650fe9adce2ee3871eb10271487fc262d8
Adina Keller <adinakeller15@gmail.com>
2025-11-02 19:29:46 +0000

    add rotate left method

--- rover_starting_point.py
+++ rover_starting_point.py
@@ -1,10 +1,25 @@
 from enums import CompassDirection
 from dataclasses import dataclass
 
+# NORTH - WEST - SOUTH - EAST - NORTH
+
 class Rover:
     def __init__(self, position):
         self.position = position
 
+    def rotate_left(self):
+            if self.position.direction == CompassDirection.NORTH:
+                self.position.direction = CompassDirection.WEST
+            elif self.position.direction == CompassDirection.WEST:
+                self.position.direction = CompassDirection.SOUTH
+            elif self.position.direction == CompassDirection.SOUTH:
+                self.position.direction = CompassDirection.EAST
+            else:
+                self.position.direction = CompassDirection.NORTH
+            return self.position.direction
+# refactor!! dont want so manual
+    
+        
     def __str__(self):
         return f'Rover: {self.position}'
         
