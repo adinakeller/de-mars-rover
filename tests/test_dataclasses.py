e07cd2050924197abe9f6815875e6d3032865be2
Adina Keller <adinakeller15@gmail.com>
2025-11-02 12:56:50 +0000

    test data classes

---
+++ tests/test_dataclasses.py
@@ -0,0 +1,31 @@
+from the_plateau import PlateauSize
+from rover_starting_point import Position
+from enums import CompassDirection
+
+def test_plateau_has_correct_grid_numbers():
+    grid = PlateauSize(width=1, height=2)
+
+    assert grid.width == 1
+    assert grid.height == 2
+
+def test_plateau_values_are_int():
+    grid = PlateauSize(width=1, height=2)
+
+    assert isinstance(grid.width, int)
+    assert isinstance(grid.height, int)
+
+def test_position_has_correct_numbers_and_direction():
+    compass_direction = CompassDirection('N')
+    position = Position(x=1, y=2, direction=compass_direction)
+
+    assert position.x == 1
+    assert position.y == 2
+    assert position.direction == CompassDirection.NORTH
+
+def test_position_data_types_are_correct():
+    compass_direction = CompassDirection('N')
+    position = Position(x=1, y=2, direction=compass_direction)
+
+    assert isinstance(position.x, int)
+    assert isinstance(position.y, int)
+    assert isinstance(position.direction, CompassDirection)
