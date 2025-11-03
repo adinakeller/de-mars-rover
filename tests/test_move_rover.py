7bd8cf8a10f238b0e9bfe32ca04c51abd53aec24
Adina Keller <adinakeller15@gmail.com>
2025-11-03 18:23:26 +0000

    test move forward method

--- tests/test_move_rover.py
+++ tests/test_move_rover.py
@@ -28,8 +28,7 @@ def test_rotate_left_from_east():
 
     assert output == CompassDirection.NORTH
     
-    
-    
+     
 def test_rotate_right_from_north():
     starting_point = Position(x=1, y=2, direction=CompassDirection('N'))
     rover = Rover(starting_point)
@@ -57,3 +56,32 @@ def test_rotate_right_from_west():
     output = rover.rotate_right()
 
     assert output == CompassDirection.NORTH
+
+
+def test_move_forward_north():
+    starting_point = Position(x=2, y=4, direction=CompassDirection('N'))
+    rover = Rover(starting_point)
+    output = rover.move_forward()
+
+    assert output == (2, 5)
+
+def test_move_forward_west():
+    starting_point = Position(x=2, y=4, direction=CompassDirection('W'))
+    rover = Rover(starting_point)
+    output = rover.move_forward()
+
+    assert output == (1, 4)
+
+def test_move_forward_south():
+    starting_point = Position(x=2, y=4, direction=CompassDirection('S'))
+    rover = Rover(starting_point)
+    output = rover.move_forward()
+
+    assert output == (2, 3)
+
+def test_move_forward_east():
+    starting_point = Position(x=2, y=4, direction=CompassDirection('E'))
+    rover = Rover(starting_point)
+    output = rover.move_forward()
+
+    assert output == (3, 4)
