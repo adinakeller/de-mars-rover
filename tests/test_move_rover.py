d1d670e79e56a19dfcbadc727c8687daa5b76881
Adina Keller <adinakeller15@gmail.com>
2025-11-13 19:02:09 +0000

    test instruction function

--- tests/test_move_rover.py
+++ tests/test_move_rover.py
@@ -1,87 +1,110 @@
-from rover_starting_point import Rover, Position, CompassDirection
+from rover_starting_point import Rover, Position, CompassDirection, Instruction
 
-def test_rotate_left_from_north():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('N'))
-    rover = Rover(starting_point)
-    output = rover.rotate_left()
+class TestRotateLeft:
+    def test_rotate_left_from_north(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('N'))
+        rover = Rover(starting_point)
+        output = rover.rotate_left()
 
-    assert output == CompassDirection.WEST
+        assert output == CompassDirection.WEST
 
-def test_rotate_left_from_west():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('W'))
-    rover = Rover(starting_point)
-    output = rover.rotate_left()
+    def test_rotate_left_from_west(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('W'))
+        rover = Rover(starting_point)
+        output = rover.rotate_left()
 
-    assert output == CompassDirection.SOUTH
+        assert output == CompassDirection.SOUTH
 
-def test_rotate_left_from_south():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('S'))
-    rover = Rover(starting_point)
-    output = rover.rotate_left()
+    def test_rotate_left_from_south(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('S'))
+        rover = Rover(starting_point)
+        output = rover.rotate_left()
 
-    assert output == CompassDirection.EAST
+        assert output == CompassDirection.EAST
 
-def test_rotate_left_from_east():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('E'))
-    rover = Rover(starting_point)
-    output = rover.rotate_left()
+    def test_rotate_left_from_east(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('E'))
+        rover = Rover(starting_point)
+        output = rover.rotate_left()
 
-    assert output == CompassDirection.NORTH
+        assert output == CompassDirection.NORTH
     
-     
-def test_rotate_right_from_north():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('N'))
-    rover = Rover(starting_point)
-    output = rover.rotate_right()
+class TestRotateRight: 
+    def test_rotate_right_from_north(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('N'))
+        rover = Rover(starting_point)
+        output = rover.rotate_right()
 
-    assert output == CompassDirection.EAST
+        assert output == CompassDirection.EAST
 
-def test_rotate_right_from_east():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('E'))
-    rover = Rover(starting_point)
-    output = rover.rotate_right()
+    def test_rotate_right_from_east(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('E'))
+        rover = Rover(starting_point)
+        output = rover.rotate_right()
 
-    assert output == CompassDirection.SOUTH
+        assert output == CompassDirection.SOUTH
 
-def test_rotate_right_from_south():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('S'))
-    rover = Rover(starting_point)
-    output = rover.rotate_right()
+    def test_rotate_right_from_south(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('S'))
+        rover = Rover(starting_point)
+        output = rover.rotate_right()
 
-    assert output == CompassDirection.WEST
+        assert output == CompassDirection.WEST
 
-def test_rotate_right_from_west():
-    starting_point = Position(x=1, y=2, direction=CompassDirection('W'))
-    rover = Rover(starting_point)
-    output = rover.rotate_right()
+    def test_rotate_right_from_west(self):
+        starting_point = Position(x=1, y=2, direction=CompassDirection('W'))
+        rover = Rover(starting_point)
+        output = rover.rotate_right()
 
-    assert output == CompassDirection.NORTH
+        assert output == CompassDirection.NORTH
 
+class TestMoveForward:
+    def test_move_forward_north(self):
+        starting_point = Position(x=2, y=4, direction=CompassDirection('N'))
+        rover = Rover(starting_point)
+        output = rover.move_forward()
 
-def test_move_forward_north():
-    starting_point = Position(x=2, y=4, direction=CompassDirection('N'))
-    rover = Rover(starting_point)
-    output = rover.move_forward()
+        assert output == (2, 5)
 
-    assert output == (2, 5)
+    def test_move_forward_west(self):
+        starting_point = Position(x=2, y=4, direction=CompassDirection('W'))
+        rover = Rover(starting_point)
+        output = rover.move_forward()
 
-def test_move_forward_west():
-    starting_point = Position(x=2, y=4, direction=CompassDirection('W'))
-    rover = Rover(starting_point)
-    output = rover.move_forward()
+        assert output == (1, 4)
 
-    assert output == (1, 4)
+    def test_move_forward_south(self):
+        starting_point = Position(x=2, y=4, direction=CompassDirection('S'))
+        rover = Rover(starting_point)
+        output = rover.move_forward()
 
-def test_move_forward_south():
-    starting_point = Position(x=2, y=4, direction=CompassDirection('S'))
-    rover = Rover(starting_point)
-    output = rover.move_forward()
+        assert output == (2, 3)
 
-    assert output == (2, 3)
+    def test_move_forward_east(self):
+        starting_point = Position(x=2, y=4, direction=CompassDirection('E'))
+        rover = Rover(starting_point)
+        output = rover.move_forward()
 
-def test_move_forward_east():
-    starting_point = Position(x=2, y=4, direction=CompassDirection('E'))
-    rover = Rover(starting_point)
-    output = rover.move_forward()
+        assert output == (3, 4)
 
-    assert output == (3, 4)
+class TestInstructions:
+    def test_instruction_L(self):
+        starting_point = Position(x=3, y=3, direction=CompassDirection('E'))
+        rover = Rover(starting_point)
+        output = rover.instruction(Instruction('L'))
+
+        assert output == CompassDirection.NORTH
+
+    def test_instruction_R(self):
+        starting_point = Position(x=3, y=3, direction=CompassDirection('S'))
+        rover = Rover(starting_point)
+        output = rover.instruction(Instruction('R'))
+
+        assert output == CompassDirection.WEST
+
+    def test_instruction_M(self):
+        starting_point = Position(x=3, y=3, direction=CompassDirection('S'))
+        rover = Rover(starting_point)
+        output = rover.instruction(Instruction('M'))
+
+        assert output == (3, 2)
